325165d157cd17e3a57a1ef1b04889edd081dca9
kaizoku-da <mazeau.jeremy2@gmail.com>
2017-11-06 13:50:02 +0100

    Changing title to "Hello World Calculator"

--- CalculatorEx/Form1.Designer.cs
+++ CalculatorEx/Form1.Designer.cs
@@ -455,7 +455,7 @@
       this.Name = "Form1";
       this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       //Window calculator title changed to Hello World Calculator"
-      this.Text = "Karim";
+      this.Text = "Hello World Calculator";
       this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
       this.groupBox1.ResumeLayout(false);
       this.groupBox2.ResumeLayout(false);
