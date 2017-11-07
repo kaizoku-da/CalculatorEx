66654adb3245d2f2aeb2250a4fcd851821ed6ce1
kaizoku-da <mazeau.jeremy2@gmail.com>
2017-11-07 11:11:21 +0100

    Adding 1 unit test...

---
+++ CalculatorEx.Test/CalculatorExTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorEx.Test
+{
+    [TestClass]
+    
+    public class CalculatorExTest
+    {
+        [TestMethod]
+        public void Hello_World_Test()
+        {
+            int tmp_result = 0;
+            int expected_result = 20;
+            int numerateur = 5;
+            int denominateur = 100;
+            tmp_result = denominateur / numerateur;
+            Assert.AreEqual(expected_result, tmp_result); 
+        }
+    }
+}
